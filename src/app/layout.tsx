35d6da5dbc1067136f3638a9ce930ae06ee1e926
Ricardo Gimenez <cpd2@santanna.g12.br>
2025-08-19 07:36:37 -0300

    fix: adiciona suppressHydrationWarning ao componente RootLayout para evitar avisos de hidratação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍽️</text></svg>'
         />
       </head>
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <BootstrapClient />
         {children}
       </body>
